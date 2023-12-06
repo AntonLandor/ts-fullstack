6656ebefecc60aba258d89ed41fac681314521d8
Anton Landor <anton.landor@consid.se>
2023-12-06 11:11:38 +0100

    Refactor page component structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { TransactionForm } from "./_components/TransactionForm";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <div className="flex flex-col  gap-2 text-center">
+        <header className="flex flex-col  gap-2 text-center">
           <h1 className="text-5xl font-extrabold leading-tight tracking-tight sm:text-[5rem]">
             Typesafe{" "}
             <span className="inline-block">
@@ -33,10 +33,12 @@ export default async function Home() {
               </a>
             </p>
           </div>
-        </div>
-        <CrudShowcase />
+        </header>
+        <main>
+          <CrudShowcase />
+        </main>
       </div>
-    </main>
+    </div>
   );
 }
 
